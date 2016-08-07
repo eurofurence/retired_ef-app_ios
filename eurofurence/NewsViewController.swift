4d82dd2159ff038fe8a260e3652808d0a7286139
Fenrikur <ef-app-ios@fenrikur.de>
2016-08-07 21:53:29 +0200

    add: rotation support for news fix: clarified the meaning of metadata displayed on news

--- eurofurence/NewsViewController.swift
+++ eurofurence/NewsViewController.swift
@@ -22,6 +22,10 @@ class NewsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func canRotate()->Bool {
+        return true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

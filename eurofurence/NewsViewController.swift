004e915954fde636d64849fb398be9816dabb3b8
Fenrikur <ef-app-ios@fenrikur.de>
2016-08-07 19:04:15 +0200

    add: last changed, area and author added to news detail view mod: switched to dynamic fonts for news views mod: reworked layout of news details mod: storyboard title for event details changed

--- eurofurence/NewsViewController.swift
+++ eurofurence/NewsViewController.swift
@@ -10,33 +10,41 @@ import UIKit
 import RealmSwift
 
 class NewsViewController: UIViewController {
-        var news = Announcement()
+    var news = Announcement()
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var areaLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var lastChangeLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-       override func viewWillAppear(animated: Bool) {
-        self.titleLabel.text = news.Title;
-        self.descriptionLabel.text = news.Content;
+    override func viewWillAppear(animated: Bool) {
+        self.titleLabel.text = news.Title
+        self.areaLabel.text = news.Area
+        self.authorLabel.text = news.Author
+        if let lastChangeDate = NSDate.dateFromISOString(news.LastChangeDateTimeUtc) {
+            self.lastChangeLabel.text = NSDateFormatter.localizedStringFromDate(lastChangeDate, dateStyle: NSDateFormatterStyle.MediumStyle, timeStyle: NSDateFormatterStyle.ShortStyle)
+        }
+        self.descriptionLabel.text = news.Content
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
